390922596598e406e3bd375fa98ed28b2046e1a8
unknown <mallek.yessmin@gmail.com>
2022-05-28 00:56:00 +0100

    divers

--- opticien-client-4577e7976a9/front/src/app/model/divers.ts
+++ opticien-client-4577e7976a9/front/src/app/model/divers.ts
@@ -1,1 +1,9 @@
-export class Divers {}
+export class Divers {
+    public id!: number;
+    public name!: String;
+    public reference!: String;
+    public prixAchat!: number;
+    public prixVente!: number;
+    public quantite!: number;
+
+}
--- opticien-client-4577e7976a9/front/src/app/monture/add-edit-monture/add-edit-monture.component.ts
+++ opticien-client-4577e7976a9/front/src/app/monture/add-edit-monture/add-edit-monture.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl } from '@angular/forms';
-import { MatDialogRef,MatDialog } from '@angular/material/dialog';
+import { FormGroup, FormControl } from '@angular/forms';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
 import { Monture } from 'src/app/model/monture';
 import { MontureService } from 'src/app/service/monture.service';
 
@@ -11,14 +11,14 @@ import { MontureService } from 'src/app/service/monture.service';
 })
 export class AddEditMontureComponent implements OnInit {
 
-  
+
   monture: Monture = new Monture();
   showAdd!: boolean;
   showUpdate!: boolean;
 
   formValue = new FormGroup({
+    name: new FormControl(''),
     reference: new FormControl(''),
-    marque: new FormControl(''),
     prixAchat: new FormControl(''),
     prixVente: new FormControl(''),
     quantite: new FormControl('')
@@ -42,7 +42,7 @@ export class AddEditMontureComponent implements OnInit {
       prixVente: this.formValue.value.prixVente,
       quantite: this.formValue.value.quantite
     }
-
+    console.log(monture);
     this.montureService.postMonture(monture)
       .subscribe(res => {
         console.log(res);
@@ -56,48 +56,48 @@ export class AddEditMontureComponent implements OnInit {
       )
   }
 
-    //Update lunette Solaire
-    updateMontureDetails() {
-      let monture = {
-        id: this.monture.id,
-        reference: this.formValue.value.reference,
-        marque: this.formValue.value.marque,
-        prixAchat: this.formValue.value.prixAchat,
-        prixVente: this.formValue.value.prixVente,
-        quantite: this.formValue.value.quantite
-      }
-      console.log(monture);
-      this.montureService.UpdateMonture(monture)
-        .subscribe(res => {
-          console.log(res);
-          alert("Monture modifier avec succès")
-          let ref = document.getElementById('cancel')
-          ref?.click();
-          this.formValue.reset();
-          this.montureService.getMonture();
-        },
-          err => { console.log(err) }
-        )
+  //Update lunette Solaire
+  updateMontureDetails() {
+    let monture = {
+      id: this.monture.id,
+      reference: this.formValue.value.reference,
+      marque: this.formValue.value.marque,
+      prixAchat: this.formValue.value.prixAchat,
+      prixVente: this.formValue.value.prixVente,
+      quantite: this.formValue.value.quantite
     }
-    clickAddMonture() {
-      this.formValue.reset();
-      this.showAdd = true;
-      this.showUpdate = false;
-    }
-    //fill in fields from lunette solaire information
-    onEdit(row: any) {
-      this.showAdd = false;
-      this.showUpdate = true;
-      console.log(row);
-      this.monture.id = row.id;
-      this.formValue.patchValue({
-        id: row.id,
-        reference: row.reference,
-        marque: row.marque,
-        prixAchat: row.prixAchat,
-        prixVente: row.prixVente,
-        quantite: row.quantite
-      })
-    }
+    console.log(monture);
+    this.montureService.UpdateMonture(monture)
+      .subscribe(res => {
+        console.log(res);
+        alert("Monture modifier avec succès")
+        let ref = document.getElementById('cancel')
+        ref?.click();
+        this.formValue.reset();
+        this.montureService.getMonture();
+      },
+        err => { console.log(err) }
+      )
+  }
+  clickAddMonture() {
+    this.formValue.reset();
+    this.showAdd = true;
+    this.showUpdate = false;
+  }
+  //fill in fields from lunette solaire information
+  onEdit(row: any) {
+    this.showAdd = false;
+    this.showUpdate = true;
+    console.log(row);
+    this.monture.id = row.id;
+    this.formValue.patchValue({
+      id: row.id,
+      reference: row.reference,
+      marque: row.marque,
+      prixAchat: row.prixAchat,
+      prixVente: row.prixVente,
+      quantite: row.quantite
+    })
+  }
 
 }
