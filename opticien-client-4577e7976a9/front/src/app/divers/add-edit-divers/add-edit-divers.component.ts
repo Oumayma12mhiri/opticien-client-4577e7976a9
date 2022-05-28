e2aaae3051ac41e08f17df31c14ea8dff2b55c02
unknown <mallek.yessmin@gmail.com>
2022-05-28 01:18:40 +0100

    validator

--- opticien-client-4577e7976a9/front/src/app/divers/add-edit-divers/add-edit-divers.component.ts
+++ opticien-client-4577e7976a9/front/src/app/divers/add-edit-divers/add-edit-divers.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Divers } from 'src/app/model/divers';
 import { DiversService } from 'src/app/service/divers.service';
